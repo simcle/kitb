c97f3ecb66b276304ae9a3a619c13d4a243cbc1e
simcle <nendisofiandy84@gmail.com>
2023-09-08 11:44:06 +0700

    update

--- app.js
+++ app.js
@@ -42,11 +42,11 @@ let sensor = {
     timestamp: ''
 };
 
-const chemin = new modbusRTU();
-chemin.connectRTUBuffered('/dev/ttyAMA3', {baudRate: 9600});
+// const chemin = new modbusRTU();
+// chemin.connectRTUBuffered('/dev/ttyAMA3', {baudRate: 9600});
 
-const flow = new modbusRTU();
-flow.connectRTUBuffered('/dev/ttyAMA0', {baudRate: 9600});
+// const flow = new modbusRTU();
+// flow.connectRTUBuffered('/dev/ttyAMA0', {baudRate: 9600});
 
 
 // GET DATA FROM DEVICE
@@ -54,17 +54,24 @@ setInterval(() => {
     let date = new Date()
     let now = Math.round(date.getTime() / 1000);
 
-    chemin.readHoldingRegisters(8, 48, (err, data) => {
-        sensor.ph = data.buffer.readFloatBE(0).toFixed(3)
-        sensor.temp = data.buffer.readFloatBE(4).toFixed(3)
-        sensor.nh3n = data.buffer.readFloatBE(24).toFixed(3)
-        sensor.cod = data.buffer.readFloatBE(48).toFixed(3)
-        sensor.tss = data.buffer.readFloatBE(88).toFixed(3)
-    })
+    // chemin.readHoldingRegisters(8, 48, (err, data) => {
+    //     sensor.ph = data.buffer.readFloatBE(0).toFixed(3)
+    //     sensor.temp = data.buffer.readFloatBE(4).toFixed(3)
+    //     sensor.nh3n = data.buffer.readFloatBE(24).toFixed(3)
+    //     sensor.cod = data.buffer.readFloatBE(48).toFixed(3)
+    //     sensor.tss = data.buffer.readFloatBE(88).toFixed(3)
+    // })
 
-    flow.readHoldingRegisters(0, 10, (err, data) => {
-        console.log(err, data)
-    })
+    // flow.readHoldingRegisters(0, 10, (err, data) => {
+    //     console.log(err, data)
+    // })
+
+    sensor.ph = Math.floor(Math.random() * 14)
+    sensor.temp = Math.floor(Math.random() * 40)
+    sensor.cod = Math.floor(Math.random() * 500)
+    sensor.tss = Math.floor(Math.random() * 300)
+    sensor.nh3n = Math.floor(Math.random() * 20)
+    sensor.debit = Math.floor(Math.random() * 150)
 
     sensor.timestamp = now
 
@@ -73,9 +80,17 @@ setInterval(() => {
     client.publish(`kitb/${device_id}/status`, 'online')
 
     client.publish(`kitb/${device_id}/sensor`, data)
-
 }, 500)
-
+// client.subscribe('kitb/3/sensor')
+// client.on('message', (topic, message) => {
+//     const sensor = JSON.parse(message.toString())
+//     console.log('subscribe', sensor.ph)
+// })
 setInterval(() => {
-    constroller.postData(sensor)
-}, 120000)
+    const date = new Date()
+    let m = date.getMinutes()
+    let s = date.getSeconds()
+    if(m%2 == 0 && s == 0) {
+        constroller.postData(sensor)
+    }
+}, 1000)
